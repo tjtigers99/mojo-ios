3570dbcdb9f3577d02cd1b188059d798f2183139
tjtigers99 <133518011+tjtigers99@users.noreply.github.com>
2025-06-16 17:07:28 -0700

    Update Supabase anon key

--- Mojo/Supabase.swift
+++ Mojo/Supabase.swift
@@ -3,5 +3,5 @@ import Supabase
 
 let supabase = SupabaseClient(
     supabaseURL: URL(string: "https://utlrtdwxjyjmlpzbyfgx.supabase.co")!,
-    supabaseKey: "YOUR_SUPABASE_ANON_KEY"
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0bHJ0ZHd4anlqbWxwemJ5Zmd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNDAwMzAsImV4cCI6MjA2NDgxNjAzMH0.-tep6vw6CPMprmyAgE3MBE64cqAJl53H3LbTs-L6JLA"
 )
